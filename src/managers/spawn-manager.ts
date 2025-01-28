71be2d022d7d0855537f8521293a62ad34dcb2ef
Francis Breidenbach <fbreidenbach@gmail.com>
2025-01-28 15:30:42 -0800

    re-start spawn manager

--- src/managers/spawn-manager.ts
+++ src/managers/spawn-manager.ts
@@ -1,5 +1,9 @@
+type SpawnList = { [spawnName: string]: StructureSpawn };
+
 export class SpawnManager {
-  public spawns: StructureSpawn[] = [];
+  public spawns: SpawnList;
+  public spawnNames: string[] = [];
+  public spawnCount: number = 0;
 
   public constructor() {
     this.spawns = Game.spawns;
