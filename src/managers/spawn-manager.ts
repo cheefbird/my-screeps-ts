b7007e0e15581948e8127072494b299c7f018c03
Francis Breidenbach <fbreidenbach@gmail.com>
2025-01-28 16:29:23 -0800

    basic SpawnManager class

--- src/managers/spawn-manager.ts
+++ src/managers/spawn-manager.ts
@@ -1,11 +1,21 @@
-type SpawnList = { [spawnName: string]: StructureSpawn };
+import { Config } from "config";
 
 export class SpawnManager {
-  public spawns: SpawnList;
+  public spawns: StructureSpawn[] = [];
   public spawnNames: string[] = [];
   public spawnCount: number = 0;
 
   public constructor() {
-    this.spawns = Game.spawns;
+    for (const [key, value] of Object.entries(Game.spawns)) {
+      this.spawns.push(value);
+      this.spawnNames.push(key);
+    }
+  }
+
+  public updateSpawns() {
+    this.spawnCount = _.size(this.spawnNames);
+    if (Config.VERBOSE) {
+      console.log(`INFO: ${this.spawnCount} spawns in room`);
+    }
   }
 }
